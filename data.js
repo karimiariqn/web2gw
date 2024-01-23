b7644ce3f4cba56e8cd4d1715bfb0ae8e72198b4
Karimi Ariq Nadindra <karimiariqnadindra@gmail.com>
2024-01-23 21:28:35 +0700

    latihan

--- data.js
+++ data.js
@@ -1,30 +1,3 @@
 const namaSaya = "Karimi";
 let usia = 10
 
-
-function generatedata() {
-    let generasi;
-    
-    if (usia > 10 && usia < 18) { 
-        generasi "generasi remaja";
-    }
-
-    else if (usia > 18 && usia < 30) {
-        generasi "generasi dewasa";
-    }
-
-    else if (usia > 30) {
-        generasi "generasi tua";
-    }
-
-    else if (usia > 2 && usia < 10) {
-        generasi "generasi anak anak";
-    }
-    
-    else {
-        generasi "generasi bayi";
-        }
-
-return console.log('generasi saya adalah'), generasi
-}
-
